0c1d4764eefc8ab68516da235d0e6b2baf714d81
mumja92 <mumja92@gmail.com>
2018-10-17 12:11:03 +0200

    Logs now automatically show class and function name of caller

--- src/modules/mumjolandia/mumjolandia_supervisor.py
+++ src/modules/mumjolandia/mumjolandia_supervisor.py
@@ -1,4 +1,7 @@
 import logging
+
+import sys
+
 from src.modules.mumjolandia.mumjolandia_thread import MumjolandiaThread
 from src.modules.mumjolandia.ui.mumjolandia_cli import MumjolandiaCli
 from src.modules.mumjolandia.ui.mumjolandia_gui import MumjolandiaGui
@@ -8,8 +11,7 @@ from queue import Queue
 class MumjolandiaSupervisor:
     def __init__(self):
         self.command_queue = Queue()
-        logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%d/%m/%Y %H:%M:%S', filename='spam.log', level=logging.DEBUG)
-        logging.debug('This message should go to the log file')
+        logging.basicConfig(format='%(asctime)s [%(levelname).1s] %(module)s::%(funcName)s --- %(message)s', datefmt='%d/%m/%Y %H:%M:%S', filename='spam.log', level=logging.DEBUG)
 
     def run_gui(self):
         logging.info('Starting GUI')
