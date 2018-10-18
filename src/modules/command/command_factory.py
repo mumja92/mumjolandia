4df6c99333a1d4b8eab540ee87385d3c08a7fb05
mumja92 <mumja92@gmail.com>
2018-10-18 13:24:57 +0200

    commands now recognize apostrophes, so it is allowed to use multi words commands

--- src/modules/command/command_factory.py
+++ src/modules/command/command_factory.py
@@ -1,11 +1,60 @@
+from enum import Enum
+
 from src.modules.command.command import Command
 
 
+class WordType(Enum):  # if word starts with ' then WordType = 1
+    start = 1   # 'word
+    none = 2    # word wo'rd
+    quoted = 3  # 'word'
+    end = 4     # word'
+
+
 class CommandFactory:
+
     @staticmethod
     def get_command(string):
-        if len(string) == 0:
-            arguments = ['']
-            return Command(arguments)
+        if len(string) == 0 or string.isspace():
+            return Command([''])
         else:
-            return Command(string.split())
+            s = string.split()
+            start_recognized_flag = 0
+            temp = []
+            arguments = []
+            for w in s:
+                if start_recognized_flag == 0:
+                    if CommandFactory.get_word_type(w) == WordType.start:
+                        start_recognized_flag = 1
+                        temp += w
+                    elif CommandFactory.get_word_type(w) == WordType.quoted:
+                        arguments.append(w[1:-1])
+                    else:
+                        arguments.append(w)
+
+                else:
+                    if CommandFactory.get_word_type(w) == WordType.end:
+                        start_recognized_flag = 0
+                        temp += ' ' + w[:-1]
+                        arguments.append(''.join(temp[1:]))
+                        temp.clear()
+                    elif CommandFactory.get_word_type(w) == WordType.quoted:
+                        temp += ' ' + w[1:-1]
+                    else:
+                        temp += ' ' + w
+            if len(temp) != 0:
+                arguments.extend(temp)
+            return Command(arguments)
+
+    @staticmethod
+    def get_word_type(word):
+        if len(word) < 2:
+            return WordType.none
+        elif word.startswith("'") and word.endswith("'"):
+            return WordType.quoted
+        elif word.startswith("'"):
+            return WordType.start
+        elif word.endswith("'"):
+            return WordType.end
+        else:
+            return WordType.none
+
--- tests/modules/command/test_commandFactory.py
+++ tests/modules/command/test_commandFactory.py
@@ -4,15 +4,27 @@ from src.modules.command.command_factory import CommandFactory
 
 class TestCommandFactory(TestCase):
     def test_get_command(self):
-        c = CommandFactory.get_command('żółć@')
-        self.assertEquals(len(c.arguments), 1)
-        self.assertEquals(c.arguments[0], 'żółć@')
-
-        c = CommandFactory.get_command('xD xDD')
-        self.assertEquals(len(c.arguments), 2)
-        self.assertEquals(c.arguments[0], 'xD')
-        self.assertEquals(c.arguments[1], 'xDD')
-
         c = CommandFactory.get_command('')
         self.assertEquals(len(c.arguments), 1)
         self.assertEquals(c.arguments[0], '')
+
+        c = CommandFactory.get_command(' ')
+        self.assertEquals(len(c.arguments), 1)
+        self.assertEquals(c.arguments[0], '')
+
+        c = CommandFactory.get_command('a ')
+        self.assertEquals(len(c.arguments), 1)
+        self.assertEquals(c.arguments[0], 'a')
+
+        c = CommandFactory.get_command("żółć@ 'drugi argument'")
+        self.assertEquals(len(c.arguments), 2)
+        self.assertEquals(c.arguments[0], 'żółć@')
+        self.assertEquals(c.arguments[1], 'drugi argument')
+
+        c = CommandFactory.get_command("it's ''a' 'crazy' 'almost a good day' xD'' ")
+        self.assertEquals(len(c.arguments), 5)
+        self.assertEquals(c.arguments[0], "it's")
+        self.assertEquals(c.arguments[1], "'a")
+        self.assertEquals(c.arguments[2], 'crazy')
+        self.assertEquals(c.arguments[3], 'almost a good day')
+        self.assertEquals(c.arguments[4], "xD''")
