211e81a21692b1577930ecdf7c941c6396aee349
mumja92 <mumja92@gmail.com>
2019-06-28 17:52:53 +0200

    Revert "Added custom logger"
    This reverts commit 6b4f665090ad8373d27a7028ae7adab5521a0890.
    
    This feature is available in default logger. No point in reinventing it.

--- src/modules/mumjolandia/config_loader.py
+++ src/modules/mumjolandia/config_loader.py
@@ -11,10 +11,9 @@ class ConfigLoader:
         config_location = 'data/config.xml'
         file = Path(config_location)
         if not file.is_file():
-            return MumjolandiaConfigObject('WARNING', True, 'xml')
+            return MumjolandiaConfigObject('DEBUG', 'xml')
         with open(config_location, 'r') as my_file:
             data = my_file.read()
         config_dict = xmltodict.parse(data)
         return MumjolandiaConfigObject(config_dict['config']['log_level'],
-                                       config_dict['config']['log_to_display'],
                                        config_dict['config']['task_io_method'])
