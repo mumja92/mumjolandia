8c8bdef9f854d8976a7b2ca3a861dc37aca1f9ed
mumja92 <mumja92@gmail.com>
2019-07-01 19:01:35 +0200

    Removed unused Weather interface file

--- src/interface/weather/weather.py
+++
@@ -1,9 +0,0 @@
-from src.interface.mumjolandia.pod_template import PODTemplate
-
-
-class Event(PODTemplate):
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
