2db5e05cb7379fb5f42dba52a613de854ff7334f
mumja92 <mumja92@gmail.com>
2019-09-19 15:21:45 +0200

    Shared preferences refactor and 'remove' function
    Added methods to delete key(s) that start with given pattern, or match
    it exactly.
    Shared preferences are also slightly refactored to improve code quality.

--- src/utils/shared_preferences.py
+++ src/utils/shared_preferences.py
@@ -9,18 +9,55 @@ class SharedPreferences:
         self.object_loader = ObjectLoaderPickle(self.filename)
 
     def get(self, name):
-        dictionary = self.object_loader.get()
-        if not isinstance(dictionary, dict):
-            logging.warning('Could not load file as dictionary: "' + self.filename + '"')
-            return None
-        try:
-            return dictionary[name]
-        except KeyError:
-            return None
+        dictionary = self.__get_dict()
+        if dictionary is None:
+            return_value = None
+        else:
+            try:
+                return_value = dictionary[name]
+            except KeyError:
+                return_value = None
+        return return_value
 
     def put(self, name, value):
+        dictionary = self.__get_dict()
+        if dictionary is None:
+            return_value = False
+        else:
+            dictionary[name] = value
+            self.object_loader.save(dictionary)
+            return_value = True
+        return return_value
+
+    def clear_key(self, key):
+        return_value = None
+        dictionary = self.__get_dict()
+        if dictionary is not None:
+            if dictionary.pop(key, None) is not None:
+                return_value = True
+            else:
+                return_value = False
+        if return_value:
+            self.object_loader.save(dictionary)
+        return return_value
+
+    def clear_starting_pattern(self, starting_pattern):
+        return_value = 0
+        dictionary = self.__get_dict()
+        if dictionary is not None:
+            for k in list(dictionary):
+                if k.startswith(starting_pattern):
+                    del dictionary[k]
+                    return_value += 1
+        if return_value != 0:
+            self.object_loader.save(dictionary)
+        return return_value
+
+    def __get_dict(self):
         dictionary = self.object_loader.get()
         if dictionary is None:
             dictionary = {}
-        dictionary[name] = value
-        self.object_loader.save(dictionary)
+        elif not isinstance(dictionary, dict):
+            logging.error('Could not load file as dictionary: "' + self.filename + '"')
+            dictionary = None
+        return dictionary
--- tests/ut/utils/test_sharedPreferences.py
+++ tests/ut/utils/test_sharedPreferences.py
@@ -3,12 +3,13 @@ import shutil
 import os
 from src.utils.shared_preferences import SharedPreferences
 from unittest import TestCase
-
+# todo: what with None and empty values?
 
 class TestSharedPreferences(TestCase):
     def __init__(self, *args, **kwargs):
         super(TestSharedPreferences, self).__init__(*args, **kwargs)
         logging.getLogger().disabled = True
+        self.preferences_location = 'data/preferences.pickle'
 
     def setUp(self):
         os.mkdir('data')
@@ -17,7 +18,7 @@ class TestSharedPreferences(TestCase):
         shutil.rmtree(os.path.abspath(os.curdir) + '/data')
 
     def test_save_and_load(self):
-        s = SharedPreferences('data/preferences.pickle')
+        s = SharedPreferences(self.preferences_location)
         self.assertEqual(s.get('important_data'), None)
         self.assertEqual(s.get('different_important_data'), None)
         s.put('important_data', 'secret_value')
@@ -26,20 +27,71 @@ class TestSharedPreferences(TestCase):
         self.assertEqual(s.get('different_data'), 'different_secret_value')
 
     def test_overwrite_data(self):
-        s = SharedPreferences('data/preferences.pickle')
+        s = SharedPreferences(self.preferences_location)
         self.assertEqual(s.get('important_data'), None)
         s.put('important_data', 'secret_value')
         s.put('important_data', 'different_value')
         self.assertEqual(s.get('important_data'), 'different_value')
 
     def test_other_instance(self):
-        s1 = SharedPreferences('data/preferences.pickle')
+        s1 = SharedPreferences(self.preferences_location)
         self.assertEqual(s1.get('important_data'), None)
         s1.put('important_data', 'secret_value')
-        s2 = SharedPreferences('data/preferences.pickle')
+        s2 = SharedPreferences(self.preferences_location)
         self.assertEqual(s2.get('important_data'), 'secret_value')
 
     def test_load_non_existing_data(self):
-        s = SharedPreferences('data/preferences.pickle')
-        s.put('important_data', 'secret_value')
+        s = SharedPreferences(self.preferences_location)
         self.assertEqual(s.get('non_existing_data'), None)
+
+    def test_clear_with_starting_pattern(self):
+        start_string = 'find_me'
+        non_start_string_first = 'ind_me'
+        non_start_string_second = 'find_m'
+
+        key1 = start_string + 'some_data'
+        key2 = start_string + '_different_data'
+        key3 = non_start_string_first + '_some_data'
+        key4 = non_start_string_second + 'some_data'
+
+        data1 = 'some_data1'
+        data2 = 'some_data2'
+        data3 = 'some_data3'
+        data4 = 'some_data4'
+
+        s = SharedPreferences(self.preferences_location)
+        s.put(key1, data1)
+        s.put(key2, data2)
+        s.put(key3, data3)
+        s.put(key4, data4)
+
+        self.assertEqual(s.get(key1), data1)
+        self.assertEqual(s.get(key2), data2)
+        self.assertEqual(s.get(key3), data3)
+        self.assertEqual(s.get(key4), data4)
+
+        s.clear_starting_pattern(start_string)
+
+        self.assertEqual(s.get(key1), None)
+        self.assertEqual(s.get(key2), None)
+        self.assertEqual(s.get(key3), data3)
+        self.assertEqual(s.get(key4), data4)
+
+    def test_clear_key(self):
+        key1 = 'delete_me'
+        key2 = 'delete_me_not'
+
+        data1 = 'some_data1'
+        data2 = 'some_data2'
+
+        s = SharedPreferences(self.preferences_location)
+        s.put(key1, data1)
+        s.put(key2, data2)
+
+        self.assertEqual(s.get(key1), data1)
+        self.assertEqual(s.get(key2), data2)
+
+        s.clear_key(key1)
+
+        self.assertEqual(s.get(key1), None)
+        self.assertEqual(s.get(key2), data2)
