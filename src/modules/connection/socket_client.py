23a4cf98303aca85440950a869456f57067e246d
mumja92 <mumja92@gmail.com>
2019-10-15 16:29:32 +0200

    Add progress prints to connection module
    Commands update and get_file are now printing current progress in
    percentage and current download speed.

--- src/modules/connection/socket_client.py
+++ src/modules/connection/socket_client.py
@@ -1,6 +1,8 @@
 import os
 import socket
 
+import time
+
 from src.modules.connection.message_factory import MessageFactory
 from src.modules.mumjolandia.config_loader import ConfigLoader
 
@@ -37,7 +39,7 @@ class SocketClient:
                                     int(ConfigLoader.get_config().server_port)))
         m_to_send = MessageFactory().get('update')
         self.socket_client.send(m_to_send.get())
-        bytes_received = self.__receive_message()
+        bytes_received = self.__receive_message(log_progress=True)
         if os.path.isfile(file_name):
             os.remove(file_name)
         with open(file_name, 'wb') as f:
@@ -54,7 +56,7 @@ class SocketClient:
                                     int(ConfigLoader.get_config().server_port)))
         m_to_send = MessageFactory().get('get ' + file_name)
         self.socket_client.send(m_to_send.get())
-        bytes_received = self.__receive_message()
+        bytes_received = self.__receive_message(log_progress=True)
         if len(bytes_received) > 0:
             if os.path.isfile(file_name):
                 os.remove(file_name)
@@ -64,11 +66,27 @@ class SocketClient:
         self.socket_client.close()
         return return_value
 
-    def __receive_message(self):
+    def __receive_message(self, log_progress=False):
+        start_time = 0
+        previous_bytes = 0
         len_bytes = b''
         while len(len_bytes) < 4:   # first 4 bytes are length of message
             len_bytes += self.socket_client.recv(1)
         bytes_received = b''
+        if log_progress:
+            print("Downloading " + str(float(int.from_bytes(len_bytes, byteorder='big', signed=False))/1024/1024) + 'MB')
+            start_time = time.time()
         while len(bytes_received) < int.from_bytes(len_bytes, byteorder='big', signed=False):
-            bytes_received += self.socket_client.recv(1024)
+            bytes_received += self.socket_client.recv(1024*1024)
+            if log_progress:
+                elapsed_time = time.time() - start_time
+                if elapsed_time > 1:
+                    self.__print_progress(len(bytes_received),
+                                          int.from_bytes(len_bytes, byteorder='big', signed=False),
+                                          " {0:.2f}".format((len(bytes_received) - previous_bytes)/1024/1024) + 'MB/s')
+                    previous_bytes = len(bytes_received)
+                    start_time += 1
         return bytes_received
+
+    def __print_progress(self, current, target, speed):
+        print(str(int(current/target*100)) + '% ' + speed)
