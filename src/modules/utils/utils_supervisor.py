c3b60d604b93959771c34265dbdc0558b743662d
mumja92 <mumja92@gmail.com>
2019-10-19 12:41:33 +0200

    Fix typo in pompejanka and add aligning
    Text is fixed and as it is long, what results in cut words when entering
    a new line, it now automatically inserts new lines to not cut words.

--- src/modules/utils/utils_supervisor.py
+++ src/modules/utils/utils_supervisor.py
@@ -1,3 +1,6 @@
+import shutil
+import textwrap
+
 from src.interface.mumjolandia.mumjolandia_response_object import MumjolandiaResponseObject
 from src.interface.mumjolandia.mumjolandia_return_value import MumjolandiaReturnValue
 from src.modules.mumjolandia.mumjolandia_supervisor import MumjolandiaSupervisor
@@ -26,7 +29,7 @@ class UtilsSupervisor(MumjolandiaSupervisor):
         return MumjolandiaResponseObject(status=MumjolandiaReturnValue.utils_get,
                                          arguments=['1. Znak krzyza\n'
                                                     '2. Podanie intencji\n'
-                                                    '3. "Ten rozaniec odmawiam na Twoja czesc, Ktolowo Rozanca Swietego\n'
+                                                    '3. "Ten rozaniec odmawiam na Twoja czesc, Krolowo Rozanca Swietego\n'
                                                     '4. Wierze w Boga, Ojcze nasz, 3x Zdrowas Maryjo, Chwala Ojcu\n'
                                                     '5. 15 tajemnic rozanca\n'
                                                     '6. Modlitwa a (pierwsze 27 dni) lub b (drugie 27 dni)\n'
@@ -35,11 +38,13 @@ class UtilsSupervisor(MumjolandiaSupervisor):
                                                     '6b. \n' +
                                                     PolishUtfToAscii.translate(self.__get_pompejanka_dziekczynna()) + '\n'
                                                     '7. Pod Twoja obrone\n'
-                                                    '8. "Krolowo rozanca swietego, modl sie za nami!"\n'
+                                                    '8. 3x "Krolowo rozanca swietego, modl sie za nami!"\n'
                                                     ])
 
     def __get_pompejanka_blagalna(self):
-        return 'Pomnij o miłosierna Panno Różańcowa z Pompejów, jako nigdy jeszcze nie słyszano, aby ktokolwiek z czcicieli Twoich, z Różańcem Twoim, pomocy Twojej wzywający, miał być przez Ciebie opuszczony. Ach, nie gardź prośbą moją, o Matko Słowa Przedwiecznego, ale przez święty Twój różaniec i przez  upodobanie, jakie okazujesz dla Twojej świątyni w Pompejach wysłuchaj mnie dobrotliwie. Amen.'
+        text = 'Pomnij o miłosierna Panno Różańcowa z Pompejów, jako nigdy jeszcze nie słyszano, aby ktokolwiek z czcicieli Twoich, z Różańcem Twoim, pomocy Twojej wzywający, miał być przez Ciebie opuszczony. Ach, nie gardź prośbą moją, o Matko Słowa Przedwiecznego, ale przez święty Twój różaniec i przez  upodobanie, jakie okazujesz dla Twojej świątyni w Pompejach wysłuchaj mnie dobrotliwie. Amen.'
+        return textwrap.fill(text, shutil.get_terminal_size().columns)
 
     def __get_pompejanka_dziekczynna(self):
-        return 'Cóż Ci dać mogę, o Królowo pełna miłości? Moje całe życie poświęcam Tobie. Ile mi sił starczy, będę rozszerzać cześć Twoją, o Dziewico Różańca Świętego z Pompejów, bo gdy Twojej pomocy wezwałem, nawiedziła mnie łaska Boża. Wszędzie będę opowiadać o miłosierdziu, które mi wyświadczyłaś. O ile zdołam będę rozszerzać nabożeństwo do Różańca Świętego, wszystkim głosić będę, jak dobrotliwie obeszłaś się ze mną, aby i niegodni, tak jak i ja, grzesznicy, z zaufaniem do Ciebie się udawali. O, gdyby cały świat wiedział jak jesteś dobra, jaką masz litość nad cierpiącymi, wszystkie stworzenia uciekałyby się do Ciebie. Amen.'
+        text = 'Cóż Ci dać mogę, o Królowo pełna miłości? Moje całe życie poświęcam Tobie. Ile mi sił starczy, będę rozszerzać cześć Twoją, o Dziewico Różańca Świętego z Pompejów, bo gdy Twojej pomocy wezwałem, nawiedziła mnie łaska Boża. Wszędzie będę opowiadać o miłosierdziu, które mi wyświadczyłaś. O ile zdołam będę rozszerzać nabożeństwo do Różańca Świętego, wszystkim głosić będę, jak dobrotliwie obeszłaś się ze mną, aby i niegodni, tak jak i ja, grzesznicy, z zaufaniem do Ciebie się udawali. O, gdyby cały świat wiedział jak jesteś dobra, jaką masz litość nad cierpiącymi, wszystkie stworzenia uciekałyby się do Ciebie. Amen.'
+        return textwrap.fill(text, shutil.get_terminal_size().columns)
