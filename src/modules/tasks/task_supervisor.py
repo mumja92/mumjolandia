b9b17c9981b63a4636652555f208c1657490f2ce
mumja92 <mumja92@gmail.com>
2019-10-27 10:31:37 +0100

    Drop unused public interface of task supervisor
    Supervisors shall only expose 'execute' method as public method.
    Initially that wasn't followed, so task supervisor had methods to 'add'
    'edit' and 'delete' tasks. As this requirement can be addressed by using
    'execute' command those methods are deleted.

--- src/modules/tasks/task_supervisor.py
+++ src/modules/tasks/task_supervisor.py
@@ -26,55 +26,6 @@ class TaskSupervisor(MumjolandiaSupervisor):
         self.tasks = None
         self.__init()
 
-    def get_tasks(self):
-        return self.tasks
-
-    def add_task(self, name):
-        try:
-            self.tasks.append(TaskFactory.get_task(name))
-        except IncorrectDateFormatException:
-            logging.warning("Task '" + name + "' not added - incorrect date format")
-            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_incorrect_date_format)
-        logging.info("Added task '" + name + "'")
-        self.__save_if_allowed()
-        return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_added, arguments=[name, len(self.tasks)-1])
-
-    def edit_task(self, task_id, new_task):
-        try:
-            self.tasks[task_id] = new_task
-            self.__save_if_allowed()
-            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_edit_ok,
-                                             arguments=[str(task_id)])
-        except IndexError:
-            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_edit_wrong_index,
-                                             arguments=[str(task_id)])
-
-    def delete_task(self, task_id):
-        # parameter comes as string. If we can parse it to int then we remove by id. If not, then by name
-        try:
-            tid = int(task_id)
-            try:
-                self.tasks.pop(tid)
-                self.__save_if_allowed()
-                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_success,
-                                                 arguments=[task_id, str(1)])
-            except IndexError:  # wrong index
-                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_incorrect_index,
-                                                 arguments=[task_id])
-        except ValueError:  # parameter type is not int
-            deleted_counter = 0
-            for t in reversed(self.tasks):  # reversing allows to remove elements on fly without breaking ids
-                if t.name == task_id:
-                    self.tasks.remove(t)
-                    deleted_counter += 1
-            if deleted_counter == 0:
-                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_incorrect_name,
-                                                 arguments=[task_id])
-            else:
-                self.__save_if_allowed()
-                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_success,
-                                                 arguments=[task_id, str(deleted_counter)])
-
     def __init(self):
         self.__add_command_parsers()
 
@@ -122,7 +73,16 @@ class TaskSupervisor(MumjolandiaSupervisor):
         if len(args) < 1:
             return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_name_not_given)
         else:
-            return self.add_task(' '.join(args[0:]))
+            name = ' '.join(args[0:])
+            try:
+                self.tasks.append(TaskFactory.get_task(name))
+            except IncorrectDateFormatException:
+                logging.warning("Task '" + name + "' not added - incorrect date format")
+                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_incorrect_date_format)
+            logging.info("Added task '" + name + "'")
+            self.__save_if_allowed()
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_added,
+                                             arguments=[name, len(self.tasks) - 1])
 
     def __command_get(self, args):  # 'get' - return today, 'get 1' - return tomorrow,get '0' - return all
         return_list = []
@@ -199,17 +159,50 @@ class TaskSupervisor(MumjolandiaSupervisor):
 
     def __command_delete(self, args):
         try:
-            return self.delete_task(args[0])
+            task_id = args[0]
         except IndexError:
             return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_incorrect_index,
                                              arguments=['none'])
+        # parameter comes as string. If we can parse it to int then we remove by id. If not, then by name
+        try:
+            tid = int(task_id)
+            try:
+                self.tasks.pop(tid)
+                self.__save_if_allowed()
+                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_success,
+                                                 arguments=[task_id, str(1)])
+            except IndexError:  # wrong index
+                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_incorrect_index,
+                                                 arguments=[task_id])
+        except ValueError:  # parameter type is not int
+            deleted_counter = 0
+            for t in reversed(self.tasks):  # reversing allows to remove elements on fly without breaking ids
+                if t.name == task_id:
+                    self.tasks.remove(t)
+                    deleted_counter += 1
+            if deleted_counter == 0:
+                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_incorrect_name,
+                                                 arguments=[task_id])
+            else:
+                self.__save_if_allowed()
+                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_delete_success,
+                                                 arguments=[task_id, str(deleted_counter)])
 
     def __command_edit(self, args):
         try:
-            date_to_finish = self.tasks[int(args[0])].date_to_finish
+            task_id = int(args[0])
+        except IndexError:  # first argument not given
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_edit_wrong_index,
+                                             arguments=['None'])
+        try:
+            new_task = TaskFactory.get_task(name=args[1], date_to_finish=self.tasks[int(args[0])].date_to_finish)
+            self.tasks[task_id] = new_task
+            self.__save_if_allowed()
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_edit_ok,
+                                             arguments=[str(task_id)])
         except IndexError:
-            date_to_finish = None
-        return self.edit_task(int(args[0]), TaskFactory.get_task(name=args[1], date_to_finish=date_to_finish))
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.task_edit_wrong_index,
+                                             arguments=[str(task_id)])
 
     def __command_set(self, args):
         try:
