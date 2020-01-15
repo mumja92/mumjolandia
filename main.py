cb918ed079197fbfd57a8ed7ea163ce9a041479f
mumja92 <mumja92@gmail.com>
2020-01-15 09:45:20 +0100

    Add 'location' command to utils
    It shows what path is provided by ConfigLoader.get_mumjolandia_location.

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import sys
 
 from src.modules.mumjolandia.mumjolandia_starter import MumjolandiaStarter
 
+# todo: this file is not added to update package; it failed once because of this.
 m = MumjolandiaStarter(sys.argv)
 m.run_cli()
--- src/modules/utils/utils_supervisor.py
+++ src/modules/utils/utils_supervisor.py
@@ -3,6 +3,7 @@ import textwrap
 
 from src.interface.mumjolandia.mumjolandia_response_object import MumjolandiaResponseObject
 from src.interface.mumjolandia.mumjolandia_return_value import MumjolandiaReturnValue
+from src.modules.mumjolandia.config_loader import ConfigLoader
 from src.modules.mumjolandia.mumjolandia_supervisor import MumjolandiaSupervisor
 from src.utils.polish_utf_to_ascii import PolishUtfToAscii
 
@@ -19,12 +20,17 @@ class UtilsSupervisor(MumjolandiaSupervisor):
 
         self.command_parsers['help'] = self.__command_help
         self.command_parsers['ip'] = self.__command_ip
-        self.command_parsers['p'] = self.__command_pompejanka
+        self.command_parsers['location'] = self.__command_location
+        self.command_parsers['l'] = self.__command_location
         self.command_parsers['pompejanka'] = self.__command_pompejanka
+        self.command_parsers['p'] = self.__command_pompejanka
 
     def __command_help(self, args):
         return MumjolandiaResponseObject(status=MumjolandiaReturnValue.utils_help,
-                                         arguments=['[p]ompejanka\n'])
+                                         arguments=['ip\n'
+                                                    '[l]ocation\n'
+                                                    '[p]ompejanka\n'
+                                                    ])
 
     def __command_ip(self, args):
         # todo: use 'with' statement and handle exceptions
@@ -35,6 +41,10 @@ class UtilsSupervisor(MumjolandiaSupervisor):
         s.close()
         return MumjolandiaResponseObject(status=MumjolandiaReturnValue.utils_get,
                                          arguments=[str(ip_address)])
+
+    def __command_location(self, args):
+        return MumjolandiaResponseObject(status=MumjolandiaReturnValue.utils_get,
+                                         arguments=[ConfigLoader.get_mumjolandia_location()])
 
     def __command_pompejanka(self, args):
         return MumjolandiaResponseObject(status=MumjolandiaReturnValue.utils_get,
