aea895ec818fe02f1f9dc34972be6d3dcf3bf37b
mumja92 <mumja92@gmail.com>
2021-02-03 18:07:48 +0100

    Fix food database handling
    Template database is emptied. Additionally database creation script
    is fixed to generate database that works from scratch. Indexes are
    fixed to be more user-friendly.

--- data/schemes/jedzonko.db.sql
+++ data/schemes/jedzonko.db.sql
@@ -1,58 +1,57 @@
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS "meal";
-CREATE TABLE IF NOT EXISTS "meal" (
-	"id_meal"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"fk_meal_type"	INTEGER NOT NULL,
-	"name"	TEXT NOT NULL UNIQUE,
-	"recipe"	TEXT,
-	FOREIGN KEY("fk_meal_type") REFERENCES "meal_type"("id_meal_type")
+CREATE TABLE IF NOT EXISTS `recipe_day` (
+	`id_recipe_day`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`fk_breakfast`	INTEGER NOT NULL,
+	`fk_second_breakfast`	INTEGER NOT NULL,
+	`fk_dinner`	INTEGER NOT NULL,
+	`fk_tea`	INTEGER NOT NULL,
+	`fk_supper`	INTEGER NOT NULL,
+	FOREIGN KEY(`fk_supper`) REFERENCES `meal_ingradients`(`id_meal_ingradients`),
+	FOREIGN KEY(`fk_second_breakfast`) REFERENCES `meal_ingradients`(`id_meal_ingradients`),
+	FOREIGN KEY(`fk_dinner`) REFERENCES `meal_ingradients`(`id_meal_ingradients`),
+	FOREIGN KEY(`fk_breakfast`) REFERENCES `meal_ingradients`(`id_meal_ingradients`),
+	FOREIGN KEY(`fk_tea`) REFERENCES `meal_ingradients`(`id_meal_ingradients`)
 );
-DROP TABLE IF EXISTS "meal_ingradients";
-CREATE TABLE IF NOT EXISTS "meal_ingradients" (
-	"id_meal_ingradients"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"fk_meal_id"	INTEGER NOT NULL,
-	"fk_ingradient_id"	INTEGER NOT NULL,
-	"fk_amount_type"	INTEGER NOT NULL,
-	"amount"	INTEGER NOT NULL,
-	FOREIGN KEY("fk_ingradient_id") REFERENCES "ingradient"("id_ingradient"),
-	FOREIGN KEY("fk_amount_type") REFERENCES "amount_type"("id_amount_type"),
-	FOREIGN KEY("fk_meal_id") REFERENCES "meal"("id_meal")
+CREATE TABLE IF NOT EXISTS `meal_type` (
+	`id_meal_type`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL UNIQUE
 );
-DROP TABLE IF EXISTS "recipe_day";
-CREATE TABLE IF NOT EXISTS "recipe_day" (
-	"id_recipe_day"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"fk_breakfast"	INTEGER NOT NULL,
-	"fk_second_breakfast"	INTEGER NOT NULL,
-	"fk_dinner"	INTEGER NOT NULL,
-	"fk_tea"	INTEGER NOT NULL,
-	"fk_supper"	INTEGER NOT NULL,
-	FOREIGN KEY("fk_tea") REFERENCES "meal_ingradients"("id_meal_ingradients"),
-	FOREIGN KEY("fk_second_breakfast") REFERENCES "meal_ingradients"("id_meal_ingradients"),
-	FOREIGN KEY("fk_dinner") REFERENCES "meal_ingradients"("id_meal_ingradients"),
-	FOREIGN KEY("fk_breakfast") REFERENCES "meal_ingradients"("id_meal_ingradients"),
-	FOREIGN KEY("fk_supper") REFERENCES "meal_ingradients"("id_meal_ingradients")
+CREATE TABLE IF NOT EXISTS `meal_ingradients` (
+	`id_meal_ingradients`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`fk_meal_id`	INTEGER NOT NULL,
+	`fk_ingradient_id`	INTEGER NOT NULL,
+	`fk_amount_type`	INTEGER NOT NULL,
+	`amount`	INTEGER NOT NULL,
+	FOREIGN KEY(`fk_amount_type`) REFERENCES `amount_type`(`id_amount_type`),
+	FOREIGN KEY(`fk_meal_id`) REFERENCES `meal`(`id_meal`),
+	FOREIGN KEY(`fk_ingradient_id`) REFERENCES `ingradient`(`id_ingradient`)
 );
-DROP TABLE IF EXISTS "meal_type";
-CREATE TABLE IF NOT EXISTS "meal_type" (
-	"id_meal_type"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"name"	TEXT NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS `meal` (
+	`id_meal`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`fk_meal_type`	INTEGER NOT NULL,
+	`name`	TEXT NOT NULL UNIQUE,
+	`recipe`	TEXT,
+	FOREIGN KEY(`fk_meal_type`) REFERENCES `meal_type`(`id_meal_type`)
 );
-DROP TABLE IF EXISTS "ingradient";
-CREATE TABLE IF NOT EXISTS "ingradient" (
-	"id_ingradient"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"name"	TEXT NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS `ingradient` (
+	`id_ingradient`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL UNIQUE
 );
-DROP TABLE IF EXISTS "amount_type";
-CREATE TABLE IF NOT EXISTS "amount_type" (
-	"id_amount_type"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"name"	TEXT NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS `amount_type` (
+	`id_amount_type`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL UNIQUE
 );
-DROP INDEX IF EXISTS "ix_meal_ingradients";
-CREATE UNIQUE INDEX IF NOT EXISTS "ix_meal_ingradients" ON "meal_ingradients" (
-	"fk_meal_id",
-	"fk_ingradient_id"
+INSERT INTO `amount_type` VALUES (1,'g');
+INSERT INTO `amount_type` VALUES (2,'number');
+INSERT INTO `amount_type` VALUES (3,'a_little');
+INSERT INTO `amount_type` VALUES (4,'ml');
+INSERT INTO `amount_type` VALUES (5,'handful');
+INSERT INTO `amount_type` VALUES (6,'slice');
+INSERT INTO `amount_type` VALUES (7,'spoon');
+CREATE UNIQUE INDEX IF NOT EXISTS `ix_meal_ingradients` ON `meal_ingradients` (
+	`fk_meal_id`,
+	`fk_ingradient_id`
 );
-DROP VIEW IF EXISTS "v_meal";
 CREATE VIEW v_meal AS 
 SELECT
  meal.name as name,
