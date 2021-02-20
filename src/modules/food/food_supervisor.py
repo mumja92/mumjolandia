ae34010aaaa5ae66f8c5f9ab1ae5b0365b95f4d2
mumja92 <mumja92@gmail.com>
2021-02-20 22:05:40 +0100

    Food refactor
    Food module now works in a different way. It is possible to list all
    existing recipes, compose a meal from them and after all list created
    meal with all required ingredients.

--- src/modules/food/food_supervisor.py
+++ src/modules/food/food_supervisor.py
@@ -4,6 +4,7 @@ from src.interface.food.ingredient import Ingredient
 from src.interface.food.recipe_day import RecipeDay
 from src.interface.mumjolandia.mumjolandia_response_object import MumjolandiaResponseObject
 from src.interface.mumjolandia.mumjolandia_return_value import MumjolandiaReturnValue
+from src.modules.food.custom_meal_handler import CustomMealHandler
 from src.modules.food.food_database_helper import FoodDatabaseHelper
 from src.modules.food.meal_factory import MealFactory
 from src.modules.mumjolandia.mumjolandia_supervisor import MumjolandiaSupervisor
@@ -12,6 +13,7 @@ from src.modules.mumjolandia.mumjolandia_supervisor import MumjolandiaSupervisor
 class FoodSupervisor(MumjolandiaSupervisor):
     def __init__(self, db_location):
         super().__init__()
+        self.custom_meal_handler = CustomMealHandler()
         self.db_location = db_location
         self.db_helper = FoodDatabaseHelper(self.db_location)
         self.is_db_ok = self.db_helper.is_database_ok()
@@ -56,9 +58,9 @@ class FoodSupervisor(MumjolandiaSupervisor):
         return m
 
     def __insert_meal(self, meal):
-        meal_id = self.db_helper.get_meal_id_if_exists(meal.name, meal.recipe, meal.type.value)
+        meal_id = self.db_helper.get_meal_id_if_exists(meal.name, meal.meal, meal.type.value)
         if meal_id is None:
-            meal_id = self.db_helper.get_new_meal_id(meal.type.value, meal.name, meal.recipe)
+            meal_id = self.db_helper.get_new_meal_id(meal.type.value, meal.name, meal.meal)
         ingredient_list = []
         for i in meal.ingredients:
             ingredient_list.append([self.db_helper.get_ingredient_id(i.name), i.amount, i.amount_type])
@@ -68,36 +70,52 @@ class FoodSupervisor(MumjolandiaSupervisor):
 
     def __add_command_parsers(self):
         self.command_parsers['get'] = self.__command_get
+        self.command_parsers['g'] = self.__command_get
         self.command_parsers['help'] = self.__command_help
-        self.command_parsers['ingredients'] = self.__command_ingredient
-        self.command_parsers['i'] = self.__command_ingredient
-        self.command_parsers['lists'] = self.__command_list
+        self.command_parsers['h'] = self.__command_help
+        self.command_parsers['list'] = self.__command_list
         self.command_parsers['l'] = self.__command_list
         self.command_parsers['ls'] = self.__command_list
+        self.command_parsers['set'] = self.__command_set_custom_meal
+        self.command_parsers['s'] = self.__command_set_custom_meal
+        self.command_parsers['meal'] = self.__command_get_custom_meal
+        self.command_parsers['m'] = self.__command_get_custom_meal
 
     def __command_list(self, args):
+        index = None
+        try:
+            index = int(args[0])
+        except (IndexError, ValueError):
+            pass
+        helper = FoodDatabaseHelper(self.db_location)
+        meals = helper.get_meals(index)
+        return_values = []
+        for meal in meals:
+            return_values.append('[' + str(meal[0]) + '][' + str(meal[1]) + '] ' + str(meal[2]))
         return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_list_ok,
-                                         arguments=[self.__get_database_indexes()])
+                                         arguments=return_values)
 
     def __command_get(self, args):
         try:
-            index = self.__get_database_indexes()[int(args[0])]
-            recipe = self.get_recipe_day(index)
-            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_get_ok, arguments=[recipe])
+            index = int(args[0])
         except (IndexError, ValueError):
-            try:
-                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_get_wrong_index,
-                                                 arguments=[args[0]])
-            except IndexError:  # if args[0] is empty (parameter not given) it will throw IndexError again
-                return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_get_wrong_index,
-                                                 arguments=[''])
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_get_wrong_index, arguments=[str(args)])
+        helper = FoodDatabaseHelper(self.db_location)
+        meal = helper.get_meal_ingredients(index)
+        recipe = helper.get_meal(index)[0][3]
+        ingredients_list = []
+        for ingredient in meal:
+            ingredients_list.append([ingredient[1:]])
+        return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_get_ok,
+                                         arguments=[[str(meal[0][0]), recipe], ingredients_list])
 
     def __command_help(self, args):
         return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_help,
                                          arguments=[
-                                             'get [id]\n'
-                                             '[i]ngredients [id]\n'
-                                             '[l]ist\n'
+                                             '[g]et {id}\n'
+                                             '[l]ist {/optional meal 1-5}\n'
+                                             '[m]eal\n'
+                                             '[s]et {recipe} {meal 0-4}\n'
                                              ''])
 
     def __command_ingredient(self, args):
@@ -132,7 +150,47 @@ class FoodSupervisor(MumjolandiaSupervisor):
                                                     (recipe.supper.name, ingredients_tea),
                                                     (recipe.tea.name, ingredients_supper)])
 
-    def __get_database_indexes(self):
+    def __command_set_custom_meal(self, args):
+        try:
+            recipe_index = int(args[0])
+            which_meal_index = int(args[1])
+        except ValueError:
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_get_wrong_index,
+                                             arguments=[str(args)])
+        if self.custom_meal_handler.set(recipe_index, which_meal_index):
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_set_ok,
+                                             arguments=[str(recipe_index),
+                                                        str(which_meal_index)])
+        else:
+            return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_get_wrong_index,
+                                             arguments=[str(recipe_index) + ' ' + str(which_meal_index)])
+
+    def __command_get_custom_meal(self, args):
+        return_value = [[], []]
+        meals_indexes = self.custom_meal_handler.get()
+        meals = []
+        for index in meals_indexes:
+            meals.append(self.db_helper.get_meal_ingredients(index))
+        ingredients = []
+        for meal in meals:
+            if meal is None:
+                continue
+            for ingredient in meal:
+                ingredients.append(ingredient[1:])
+        ingredients.sort()
+        for i in range(0, 5):
+            if meals_indexes[i] is None:
+                return_value[0].append([None, None])
+            else:
+                return_value[0].append([meals_indexes[i], meals[i][0][0]])
+        return_value[1] = ingredients
+        return MumjolandiaResponseObject(status=MumjolandiaReturnValue.food_meal_ok,
+                                         arguments=return_value)
+
+
+
+
+    def __get_database_indexes(self, args):
         return_value = []
         for x in self.db_helper.get_recipes_ids():
             return_value.append(x[0])
