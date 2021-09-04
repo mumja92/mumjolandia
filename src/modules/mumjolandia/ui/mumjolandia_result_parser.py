0b8d36cdd06f6d7351b352b5400c027340e5f2c0
mumja92 <mumja92@gmail.com>
2021-09-04 14:45:48 +0200

    Enable 'task find' command for server
    It now returns string instead of task objects.

--- src/modules/mumjolandia/ui/mumjolandia_result_parser.py
+++ src/modules/mumjolandia/ui/mumjolandia_result_parser.py
@@ -5,8 +5,9 @@ from src.interface.planner.plan import Plan
 class MumjolandiaResultParser:
     def __init__(self):
         self.response_dict = {'default': self.__parse_default,
-                              MumjolandiaReturnValue.task_get.name: self.__parse_task_ls,
                               MumjolandiaReturnValue.planner_get_ok.name: self.__parse_planner_get_ok,
+                              MumjolandiaReturnValue.task_find.name: self.__parse_task_find,
+                              MumjolandiaReturnValue.task_get.name: self.__parse_task_ls,
                               }
 
     def parse(self, mumjolandia_response_object):
@@ -18,12 +19,6 @@ class MumjolandiaResultParser:
     def __parse_default(self, response_object):
         return str(response_object.status) + "\n" + str(response_object.arguments)
 
-    def __parse_task_ls(self, response_object):
-        return_value = str(response_object.status) + "\n"
-        for index, task in zip(response_object.arguments[0], response_object.arguments[1]):
-            return_value += str(index) + "\t" + str(task) + "\n"
-        return return_value
-
     def __parse_planner_get_ok(self, response_object):
         return_value = ""
         return_value += str(response_object.status) + "\n"
@@ -32,4 +27,16 @@ class MumjolandiaResultParser:
         else:   # if there is more plans - arg[0] is list object with plans
             for plan in response_object.arguments[0]:
                 return_value += str(plan)
+        return return_value
+
+    def __parse_task_find(self, response_object):
+        return_value = str(response_object.status) + "\n"
+        for index, task in zip(response_object.arguments[0], response_object.arguments[1]):
+            return_value += str(index) + "\t" + str(task) + "\n"
+        return return_value
+
+    def __parse_task_ls(self, response_object):
+        return_value = str(response_object.status) + "\n"
+        for index, task in zip(response_object.arguments[0], response_object.arguments[1]):
+            return_value += str(index) + "\t" + str(task) + "\n"
         return return_value
